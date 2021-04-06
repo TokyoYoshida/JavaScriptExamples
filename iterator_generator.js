b797cc0b6924651b925cea7d8adb54d81bdd9e50
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-04-07 06:41:24 +0900

    fix

--- iterator_generator.js
+++ iterator_generator.js
@@ -1,57 +1,70 @@
 // Iterator / Generator example
 
 // Iterator
-{
-makeIterator1 = () => {
-  let index = 0
-  const myIterator = {
-    next: () => {
-      if (index <= 10) {
-        const result = { value: index, done: false }
-        index += 1
-        return result
+
+(() => {
+  // Simple Iterator
+  console.log('## Simple Iterator ')
+  const makeIterator = () => {
+    let index = 0
+    const myIterator = {
+      next: () => {
+        if (index <= 10) {
+          const result = { value: index, done: false }
+          index += 1
+          return result
+        }
+        return { value: index, done: true }
       }
-      return { value: index, done: true }
     }
+    return myIterator
   }
-  return myIterator
-}
 
+  const it = makeIterator()
 
+  it.next()
+  const r = it.next()
+  console.log('★DEBUG ', r)
+})();
 
-// Iterable Object
+(() => {
+  // Iterable Object
+  console.log('## Iterable Object')
+  let it = {}
 
-let it2 = {}
-
-// Iterable object needs [Symbol.iterator]() method returns iterator.
-it[Symbol.iterator] = () => {
-  let index = 0
-  const myIterator = {
-    next: () => {
-      if (index <= 10) {
-        const result = { value: index, done: false }
-        index += 1
-        return result
+  // Iterable object needs [Symbol.iterator]() method returns iterator.
+  it[Symbol.iterator] = () => {
+    let index = 0
+    const myIterator = {
+      next: () => {
+        if (index <= 10) {
+          const result = { value: index, done: false }
+          index += 1
+          return result
+        }
+        return { value: index, done: true }
       }
-      return { value: index, done: true }
     }
+    return myIterator
   }
-  return myIterator
-}
 
-for (const x of it2) {
-  console.log('★DEBUG ',x)
-}
+  for (const x of it) {
+    console.log('★DEBUG ',x)
+  }
+})();
 
-// Generator function
+(() => {
+  // Generator function
+  console.log('## Generator function')
 
-function* makeIterator2() {
-  yield 1
-  yield 2
-}
+  function* makeIterator() {
+    yield 1
+    yield 2
+  }
 
-const it3 = makeIterator2()
+  const it = makeIterator()
 
-for (const x of it3) {
-  console.log('★DEBUG ', x)
-}
+  for (const x of it) {
+    console.log('★DEBUG ', x)
+  }
+})();
